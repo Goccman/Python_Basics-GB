ea9579a739dbd514faefd4f8542710f4ebfdf72f
gocman <andreyrbn@gmail.com>
2023-02-07 21:32:23 +0600

    add task 10

--- Homework_2/task_10.py
+++ Homework_2/task_10.py
@@ -8,3 +8,20 @@
 # 5 -> 1 0 1 1 0
 # 2
 
+from random import randint
+
+n = int(input("Введите количество монеток: "))
+coins = []
+for i in range(n):
+    coins.append(randint(0, 1))
+print(coins)
+
+zero = coins.count(0)
+one = coins.count(1)
+
+if zero < one:
+    print(f"Нужно перевернуть {zero} монет.")
+elif zero == one:
+    print(f"Количество монет одинаковое и равно {zero} - можно перевернуть любую сторону")
+else:
+    print(f"Нужно перевернуть {one} монет.")
